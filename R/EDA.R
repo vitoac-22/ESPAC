322df78525e12152967643d74ac1997deaef7c81
vitoac-22 <victoracaro13@hotmail.com>
2024-10-25 11:12:07 -0500

    Settings: 1 using sapply for obtain labels of our columns 2 obtain documentation

---
+++ R/EDA.R
@@ -0,0 +1,18 @@
+# Libraries
+library(rio)
+library(tidyverse)
+library(dplyr)
+library(dlookr)
+library(ggplot2)
+library(writexl)
+
+# Knowing the data
+dt <- import("DATA/USING/cpnac2023.sav")
+
+###
+# -----------
+# Variables with labels
+labels_list <- sapply(dt, function(x) attr(x, "labels"))
+labels_list <- labels_list[!sapply(labels_list, is.null)]
+# -----------
+###
